56722ab2cd7c05bfc6cd22caedd918b19f768f35
hafiz-enginer <hafiz.enginer@gmail.com>
2024-06-03 15:03:43 +0500

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,12 @@
+import qrcode
+import image 
+
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+
+data = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("qrcode.png")
